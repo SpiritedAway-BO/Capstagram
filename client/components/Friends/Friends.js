530c6e53f0f02c7544ce3e1bae4999703bb8d27f
Tessa Thornberry <tthornberryclass@gmail.com>
2022-11-21 15:05:26 -0800

    added friends page info

--- client/components/Friends/Friends.js
+++ client/components/Friends/Friends.js
@@ -7,62 +7,39 @@ import { Stack, Avatar } from '@react-native-material/core';
 var DATA = [{
   id: 1234567,
   username: 'thisGuy',
-  caption: 'It\'s the little things in life',
-  upvotes: 5,
   usericon: '../../assets/favicon.png',
-  voted: true,
-  timestamp: Date()
 },
 {
   id: 1234568,
   username: 'thisGuy2',
-  caption: 'Let me show you my Pokemon!',
-  upvotes: 15,
   usericon: '../../assets/favicon.png',
-  voted: false
 },
 {
   id: 1234569,
   username: 'thisGuy3',
-  caption: 'Are we there yet?',
-  upvotes: 0,
   usericon: '../../assets/favicon.png',
-  voted: false
+
 },
 {
   id: 1234570,
   username: 'thisGuy4',
-  caption: 'Let me show you the world!',
-  upvotes: 33,
   usericon: '../../assets/favicon.png',
-  voted: true
 },
 {
   id: 1234571,
   username: 'thisGuy5',
-  caption: 'All your base are belongs to us!',
-  upvotes: 2,
   usericon: '../../assets/favicon.png',
-  voted: false
+
 },
 {
   id: 1234572,
   username: 'thisGuy6',
-  caption: 'I can haz cheezeburger?',
-  upvotes: 4,
   usericon: '../../assets/favicon.png',
-  voted: false
 }];
 
-const CaptionItem = ({ caption }) => {
-  const [voted, setVoted] = useState(caption.voted);
-  const [votes, setVotes] = useState(caption.upvotes);
-
-  useEffect(() => {
-    setVotes(caption.upvotes);
-  }, []);
-
-  //will need to send put/patch state to database? or somehow keep track
+const FriendItem = ({ caption }) => {
+  /*this would be where we could keep track of state for the onPress handler
+  that would potentially take us to the feed with onlly that friend's photos or captions, depending on what we decide to prioritize*/
   return (
     <View style={styles.item}>
       <View style={styles.userInfo} >
@@ -72,33 +49,14 @@ const CaptionItem = ({ caption }) => {
         />
         <Text style={styles.title}>{caption.username}</Text>
       </View>
-      <Text style={styles.title}>{caption.caption}</Text>
-      { voted ?
-        <View style={styles.heartIcon} >
-          <Text style={styles.title}>{votes}</Text>
-          <Ionicons name="ios-heart" size={30} color="#FF842B"
-            onPress={() => {
-              setVotes(votes - 1);
-              setVoted(!voted);
-            }}/>
-        </View> :
-        <View style={styles.heartIcon} >
-          <Text style={styles.title} >{votes}</Text>
-          <Ionicons style={styles.heartIcon} name="ios-heart-outline" size={30} color="#FF842B"
-            onPress={() => {
-              setVotes(votes + 1);
-              setVoted(!voted);
-            }}
-          />
-        </View>}
     </View>
   );
 };
 
 const Friends = () => {
 
-  const renderCaption = ({ item }) => (
-    <CaptionItem caption={item} />
+  const renderFriend = ({ item }) => (
+    <FriendItem caption={item} />
   );
 
   return (
@@ -106,7 +64,7 @@ const Friends = () => {
       <View style={styles.captionBox}>
         <FlatList //this is like map
           data={DATA}
-          renderItem={renderCaption}
+          renderItem={renderFriend}
           keyExtractor={item => item.id}
         />
       </View>
@@ -120,23 +78,16 @@ const styles = StyleSheet.create({
     marginTop: StatusBar.currentHeight || 0,
     backgroundColor: '#f9c2ff',
   },
-  captionBox: {
-    backgroundColor: '#f9c2ff',
-    // padding: 20,
-    // margin: 20,
-  },
+  // captionBox: {
+  //   backgroundColor: '#f9c2ff',
+  // },
   userInfo: {
     flexDirection: 'row',
     justifyContent: 'flex-start',
   },
-  heartIcon: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-  },
   item: {
     backgroundColor: '#fff',
     padding: 20,
-    // marginHorizontal: 16,
   },
   title: {
     fontSize: 24,
@@ -145,7 +96,6 @@ const styles = StyleSheet.create({
   avatar: {
     borderRadius: '50%',
   },
-
 });
 
 export default Friends;
