7c6cb4f855071c9a28d7e0177a48c7d1f090f4fb
Tessa Thornberry <tthornberryclass@gmail.com>
2022-11-21 10:43:51 -0800

    added base FlatList code

--- client/components/CaptionsGalore/CaptionsGalore.js
+++ client/components/CaptionsGalore/CaptionsGalore.js
@@ -8,42 +8,46 @@ var DATA = [{
   upvotes: 5,
   usericon: './assets/favicon.png',
   voted: true
+},
+{
+  id: 1234568,
+  username: 'thisGuy',
+  caption: 'Let me show you my Pokemon!',
+  upvotes: 15,
+  usericon: './assets/favicon.png',
+  voted: false
 }];
 
-const CaptionTitle = ({caption}) => (
+const Item = ({ caption }) => (
   <View style={styles.item}>
-    <Text style={styles.caption}>{caption}</Text>
+    <Text style={styles.title}>{caption}</Text>
+    <Text style={styles.title}>{caption}</Text>
   </View>
 );
 
-export default function CaptionsGalore() {
-  console.log('here');
+const CaptionsGalore = () => {
 
-  const renderCaption = ({caption}) => {
-    console.log('caption');
-    return (
-
-      <CaptionTitle caption={caption.caption} />
-    );
-  };
+  const renderItem = ({ item }) => (
+    <Item caption={item.caption} />
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList
+      <FlatList //this is like map
         data={DATA}
-        renderCaption={renderCaption}
-        keyExtractor={(caption) => caption.id}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
       />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight || 0,
   },
-  caption: {
+  item: {
     backgroundColor: '#f9c2ff',
     padding: 20,
     marginVertical: 8,
@@ -54,13 +58,62 @@ const styles = StyleSheet.create({
   },
 });
 
-// container: {
-//   minWidth: '100%', //makes banner as wide as the screen
-//   top: 0, //this moves the title away from the top
-//   padding: 10, //this makes the
-//   backgroundColor: 'black',
-//   color: 'white',
-//   fontSize: 25,
-//   textAlign: 'center',
-//   marginTop: -20, //reduces the overhead padding between the header and the Apptop
-// },
+export default CaptionsGalore;
+
+// const CaptionTitle = ({caption}) => {
+//   console.log('caption', caption);
+//   return (
+//     <View style={styles.item}>
+//       <Text style={styles.caption}>{caption}</Text>
+//     </View>
+//   );
+// };
+
+
+// export default function CaptionsGalore() {
+//   console.log('here');
+
+//   const renderCaption = ({caption}) => {
+//     console.log('caption');
+//     return (
+//       <CaptionTitle caption={caption.caption} />
+//     );
+//   };
+
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <FlatList
+//         data={DATA}
+//         renderCaption={renderCaption}
+//         keyExtractor={(caption) => caption.id}
+//       />
+//     </SafeAreaView>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     marginTop: StatusBar.currentHeight || 0,
+//   },
+//   caption: {
+//     backgroundColor: '#f9c2ff',
+//     padding: 20,
+//     marginVertical: 8,
+//     marginHorizontal: 16,
+//   },
+//   title: {
+//     fontSize: 32,
+//   },
+// });
+
+// // container: {
+// //   minWidth: '100%', //makes banner as wide as the screen
+// //   top: 0, //this moves the title away from the top
+// //   padding: 10, //this makes the
+// //   backgroundColor: 'black',
+// //   color: 'white',
+// //   fontSize: 25,
+// //   textAlign: 'center',
+// //   marginTop: -20, //reduces the overhead padding between the header and the Apptop
+// // },
