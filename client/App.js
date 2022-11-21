e9106b846ae8fafe3836e26536ce397daa1bd683
ryan-leigh <ryanleigh25@gmail.com>
2022-11-21 10:14:32 -0800

    Added useFriends hooks

--- client/App.js
+++ client/App.js
@@ -1,19 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import { AppBar } from '@react-native-material/core';
-
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import AccountPage from './components/AccountPage/AccountPage.js';
+
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.safeContainer}>
-      <View style={styles.topBarView}>
-        <AppBar title="Capstagram" color="black" position="sticky" trailing={<Text color='white'>'hi'</Text>} style={styles.appBar}/>
-      </View>
-      <View style={styles.container}>
-        <AccountPage style={styles.userPage}/>
-      </View>
-    </SafeAreaView>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaView style={styles.safeContainer}>
+        <View style={styles.topBarView}>
+          <AppBar title="Capstagram" color="black" position="sticky" trailing={<Text color='white'>'hi'</Text>} style={styles.appBar}/>
+        </View>
+        <View style={styles.container}>
+          <AccountPage style={styles.userPage}/>
+        </View>
+      </SafeAreaView>
+    </QueryClientProvider>
   );
 }
 
--- client/components/Search/Search.js
+++ client/components/Search/Search.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Image, View, Platform, TouchableOpacity, Text, StyleSheet } from 'react-native';
+import useFriends from '../../hooks/useFriends.js';
 
 export default function Search() {
+  const {{isLoading: useFriendsGetIsLoading}, {data: friends}} = useFriends.get();
+  const {{isLoading: useFriendsAddIsLoading}, isSuccess, isFailure, {mutate: useFriendsAddMutate}} = useFriends.add();
+
+  if (useFriendsGetIsLoading) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+
   return (
     <View>
       <Text>Hello</Text>
+      {/* {friends.map((friend) => (
+        <View>
+          <Text>{friend.name}</Text>
+        </View>
+      ))} */}
+      {/* <Button onClick={(data) => useFriendsAddMutate(data)}>Add a friend!</Button> */}
     </View>
   );
 }
