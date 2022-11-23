08236bf62f7f65a26df213710783f77cdea958cd
Tessa Thornberry <tthornberryclass@gmail.com>
2022-11-23 14:19:48 -0800

    updated firebase config

--- client/components/Auth/firebase/firebase.js
+++ client/components/Auth/firebase/firebase.js
@@ -2,22 +2,16 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
 import { getStorage, ref } from 'firebase/storage';
 
-import {REACT_APP_FIREBASE_KEY, REACT_APP_FIREBASE_DOMAIN, REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_STORAGE_BUCKET, REACT_APP_FIREBASE_SENDER_ID, REACT_APP_FIREBASE_APP_ID,} from '@env';
+import {REACT_APP_FIREBASE_KEY, REACT_APP_FIREBASE_DOMAIN, REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_STORAGE_BUCKET, REACT_APP_FIREBASE_SENDER_ID, REACT_APP_FIREBASE_APP_ID,} from './config.js';
 
 
 const firebaseConfig = {
-  // apiKey: REACT_APP_FIREBASE_KEY,
-  // authDomain: REACT_APP_FIREBASE_DOMAIN,
-  // projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  // storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: REACT_APP_FIREBASE_SENDER_ID,
-  // appId: REACT_APP_FIREBASE_APP_ID,
-  apiKey: "AIzaSyCYCEjorfAqvvP-wkf9cEayym6bKVndd7g",
-  authDomain: "capstagram-c0e24.firebaseapp.com",
-  projectId: "capstagram-c0e24",
-  storageBucket: "capstagram-c0e24.appspot.com",
-  messagingSenderId: "694363628131",
-  appId: "1:694363628131:web:3625bae0a0c75275ad8e56",
+  apiKey: REACT_APP_FIREBASE_KEY,
+  authDomain: REACT_APP_FIREBASE_DOMAIN,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_FIREBASE_SENDER_ID,
+  appId: REACT_APP_FIREBASE_APP_ID,
   // databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
 };
 
