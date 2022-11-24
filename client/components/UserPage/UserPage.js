a2c1ea71b29f2908bb3c6691697acc9ac004a9dd
Roshan Patel <roshanupatel@gmail.com>
2022-11-23 18:25:41 -0800

    UserPage CSS scaffolding complete

--- client/components/UserPage/UserPage.js
+++ client/components/UserPage/UserPage.js
@@ -1,122 +1,105 @@
 import React from 'react';
 import { SafeAreaView, FlatList, View, ScrollView, StyleSheet, Text, Image, Dimensions} from 'react-native';
-import { Avatar } from '@react-native-material/core';
+import { Avatar, Divider } from '@react-native-material/core';
 
-const x = {uri: 'https://img.freepik.com/free-vector/cute-rabbit-with-duck-working-laptop-cartoon-illustration_56104-471.jpg?w=2000'};
-const DATA = [x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x, x];
+var x = {
+  uri: "https://img.freepik.com/free-vector/cute-rabbit-with-duck-working-laptop-cartoon-illustration_56104-471.jpg?w=2000"
+};
+var DATA = [x, x, x, x, x, x, x, x ];
 
 const UserPage = () => {
 
-  const renderImage = (imageURI) => (
-    <View>
+  const renderImage = ({item}) => (
+    <View style={{}}>
       <Image
-        source={{uri: imageURI}}
+        source={item}
         style={styles.image}
       />
     </View>
   );
 
   return (
-    <View contentContainerStyle={styles.userPageContainer} style={styles.userPCon}>
-      <View style={styles.userInfoContainer}>
-        <View style={styles.uiStatsBox}>
-          <Avatar
-              image={{ uri: 'https://mui.com/static/images/avatar/1.jpg' }}
-              size={100}
-              style={styles.avatar}/>
-          <View style={styles.postStats}>
-            <Text>Posts: 352095</Text>
-          </View>
-          <View style={styles.friendStats}>
-            <Text>Friends: 1</Text>
-          </View>
+    <>
+    <View style={styles.userInfoContainer}>
+      <View style={styles.uiStatsBox}>
+        <Avatar
+            image={{ uri: 'https://mui.com/static/images/avatar/1.jpg' }}
+            size={80}
+            style={styles.avatar}/>
+        <View style={styles.postStats}>
+          <Text>Posts: 352095</Text>
         </View>
-        <View style={styles.aboutMeBox}>
-          <View style={styles.aboutMeLabel}>
-            <Text>About me:</Text>
-          </View>
-          <View style={styles.aboutMeLabel}>
-            <Text>text here ..................................................................................</Text>
-          </View>
-        </View>
-        <View style={styles.userPageNavBar}>
-          <View style={styles.uPNVB}></View>
+        <View style={styles.friendStats}>
+          <Text>
+            Friends: 1
+          </Text>
         </View>
       </View>
-       {/* <Divider style={styles.divider} leadingInset={16} /> */}
-       <View style={styles.postsGridContainer}>
-        <View style={styles.imageScroll}>
-            <FlatList
-              numColumns={3}
-              contentContainerStyle={styles.ccFlatList}
-              style={styles.imageList}
-              data={DATA}
-              renderItem={item => renderImage(item.uri)}
-              keyExtractor={(item, index) => index.toString()}
-            />
-        </View>
+      <View style={styles.aboutMe}>
+        <Text style={{textDecorationLine: "underline"}}>
+          About me
+        </Text>
+      </View>
+      <View style={styles.aboutMeText}>
+        <Text>
+          This is a long, drawn out sentence that gives superficial, probably not true information about me.
+        </Text>
+      </View>
+
+    </View>
+    <View style={styles.userPageNavBar}>
+      <View style={{flex: 1, alignSelf: "center"}}>
+        <Text style={{alignSelf: "center", textDecorationLine: "underline"}}>Personal Wins</Text>
+      </View>
+      <View style={{flex: 1, alignSelf: "center"}}>
+        <Text style={{alignSelf: "center", textDecorationLine: "underline"}}>Friends</Text>
       </View>
     </View>
+    <Divider style={{ marginTop: 0, marginBottom: 5 }} color="purple" leadingInset={20} trailingInset={20}/>
+    <FlatList
+      numColumns={3}
+      contentContainerStyle={styles.ccFlatList}
+      // columnWrapperStyle={{justifyContent: 'space-between'}}
+      style={styles.imageList}
+      data={DATA}
+      renderItem={renderImage}
+      keyExtractor={(item, index) => index.toString()}
+    />
+    </>
   )
 }
 
 const styles = StyleSheet.create({
-  userPageContainer: {
-    width: "100%",
-    height: "100%",
-  },
-  userPCon: {
-    width: "100%",
-    height: "100%",
-
-    borderWidth: "2px",
-    borderStyle: "solid",
-    borderRadius: "5px",
-  },
   userInfoContainer: {
-    height: Dimensions.get('window').height/3,
-
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderRadius: "10px",
+    // height: Dimensions.get('window').height/3,
+    flex: .4,
+    padding: "2%",
   },
   divider : {
     marginTop: "1%",
-  },
-  postsGridContainer: {
-    height: Dimensions.get('window').height/1.7,
-    width: "100%",
-
-    borderWidth: "1px",
-    borderStyle: "solid",
-    borderRadius: "10px",
   },
   scrollContainer: {
     flexDirection: "row",
     flexWrap: "wrap",
   },
   ccFlatList: {
-    // justifyContent:"space-around",
-    // alignItems:"center",
-  },
-  imageScroll: {
-    padding: ".1%",
-    flexDirection: "row",
-    flexWrap: "wrap",
+    //justifyContent:"space-around",
+    alignItems: "flex-start",
+    width: "100%",
   },
   imageList: {
+    flex: 2,
     width: "100%",
     flexDirection: "row",
     flexWrap: "wrap",
     overflow: "hidden",
+    padding: "1%",
   },
   image: {
-    width: Dimensions.get('window').width/4.08,
-    height: Dimensions.get('window').height/4.5,
-    alignItems:"center",
-
-    borderWidth: ".5px",
-    borderRadius: "10px",
+    width: Dimensions.get('window').width/3.14,
+    height: Dimensions.get('window').height/4,
+    marginRight: Dimensions.get('window').width*0.01,
+    marginBottom: Dimensions.get('window').width*0.01,
   },
   uiStatsBox: {
     flex: 1,
@@ -128,44 +111,24 @@ const styles = StyleSheet.create({
   postStats: {
     alignSelf: "center",
 
-    borderWidth: ".5px",
-    borderRadius: "10px",
   },
   friendStats: {
     alignSelf: "center",
 
-    borderWidth: ".5px",
-    borderRadius: "10px",
   },
   avatar: {
-
-
-    borderWidth: ".5px",
-    borderRadius: "10px",
   },
-  aboutMeBox: {
-    // alignSelf: "center",
-    // justifyContent: "center",
-    flexDirection: "row",
-    flex: 1,
-    width: "100%",
-
-    borderWidth: ".5px",
-    borderRadius: "10px",
-  },
-  aboutMeLabel: {
-    borderWidth: ".5px",
-    borderRadius: "10px",
+  aboutMe: {
+    alignSelf: "left",
+    justifyContent: "center",
+    marginLeft: "8%",
   },
   aboutMeText: {
-    borderWidth: ".5px",
-    borderRadius: "10px",
   },
   userPageNavBar: {
-    flex: .5,
-
-    borderWidth: ".5px",
-    borderRadius: "10px",
+    justifyContent: "space-between",
+    flex: .1,
+    flexDirection: "row",
   }
 });
 
