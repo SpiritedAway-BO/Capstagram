3b4002bbf94baa854b8be744a226252d8a374707
Roshan Patel <roshanupatel@gmail.com>
2022-11-26 10:38:38 -0800

    Username Plus icon fixed

--- client/components/Auth/firebase/firebase.js
+++ client/components/Auth/firebase/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, updateProfile } from 'firebase/auth';
 import { getStorage, ref } from 'firebase/storage';
-// import { REACT_APP_FIREBASE_KEY, REACT_APP_FIREBASE_DOMAIN, REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_STORAGE_BUCKET, REACT_APP_FIREBASE_SENDER_ID, REACT_APP_FIREBASE_APP_ID, } from './config.js';
+import { REACT_APP_FIREBASE_KEY, REACT_APP_FIREBASE_DOMAIN, REACT_APP_FIREBASE_PROJECT_ID, REACT_APP_FIREBASE_STORAGE_BUCKET, REACT_APP_FIREBASE_SENDER_ID, REACT_APP_FIREBASE_APP_ID, } from './config.js';
 
 const firebaseConfig = {
   apiKey: REACT_APP_FIREBASE_KEY,
