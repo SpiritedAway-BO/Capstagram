1eb4c8259889fe97b55e0668a255a4f19d0b970e
Tessa Thornberry <tthornberryclass@gmail.com>
2022-11-27 14:16:40 -0800

    merging

--- client/components/AddPhoto/AddPhotoCloudinary.js
+++ client/components/AddPhoto/AddPhotoCloudinary.js
@@ -64,11 +64,7 @@ const AddPhotoCloudinary = ({ navigation }) => {
       .then(data => {
         console.log('response data', data);
         // setPhoto(data.secure_url);
-<<<<<<< HEAD
-        axios.post('https://shaggy-streets-act-75-80-43-25.loca.lt/photos', {
-=======
         axios.post('http://localhost:8000/photos', {
->>>>>>> main
           currentUser: auth.currentUser,
           uri: data.secure_url
         })
