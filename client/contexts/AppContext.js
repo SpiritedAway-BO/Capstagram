5b720a5f4109e7c0dfde3e95ba29016cde60e05c
Stephanie <stephaniezjchen@gmail.com>
2022-11-26 20:07:34 -0500

    use req.params instead of req.body

--- client/contexts/AppContext.js
+++ client/contexts/AppContext.js
@@ -1,4 +1,5 @@
 import { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 
 export const AppContext = createContext(null);
 
@@ -6,6 +7,12 @@ export const AppProvider = ({ children }) => {
   const [username, setUsername] = useState('stephc123');
 
   const value = { username, setUsername };
+
+  useEffect(() => {
+    axios.get('https://thick-hoops-warn-99-227-192-34.loca.lt/photos/VwyMkBrrLsP047lPEfWqXAIKdDk1')
+      .then(res => console.log(res.data))
+      .catch(err => console.log(err));
+  }, []);
 
   return (
     <AppContext.Provider value={value}>
